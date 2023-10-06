ef75beaf2267245cf9d135065c6a896ce55c11c1
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-10-06 23:10:21 +0530

    begin

---
+++ Project_Back/src/test/java/com/Pro1/Project_Back/ProjectBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pro1.Project_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
