ce371bd8e3a864b0af2f92d84f4670a474883c60
Madhubhashana Mahindarathne <104136158+Madhubhashana99@users.noreply.github.com>
2023-11-10 19:34:25 +0530

    update

--- Login-SpringBoot/src/main/java/com/example/LoginSpringBoot/Model/User.java
+++ Login-SpringBoot/src/main/java/com/example/LoginSpringBoot/Model/User.java
@@ -1,4 +1,29 @@
 package com.example.LoginSpringBoot.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name= "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(nullable = true)
+    private String email;
+
+    @Column(nullable = true)
+    private String password;
 }
